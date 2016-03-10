37bffa3b694f18737f4404088e7a97bd470f5aef
fieldsfarmer <fieldsfarmer@gmail.com>
2016-03-09 18:36:23 -0600

    1.1

--- max_subarray_product.cpp
+++ max_subarray_product.cpp
@@ -6,14 +6,8 @@ using namespace std;
 // not very good
 #define GETSIZE(array) (sizeof((array))/sizeof((array[0])))
 
+// result ensure to be at least 1;
 int maxSubarrayProduct(int a[], int n){
-	// int * dp = new int[n]; 
-
-	// for(int i=0; i<n; ++i){
-	// 	dp[i] = INT_MIN;
-	// }
-	// delete[] dp;
-
 	int max_here = 1;
 	int min_here = 1;
 	int res = 1;
@@ -37,14 +31,40 @@ int maxSubarrayProduct(int a[], int n){
 	return res;
 }
 
+// result ensured to be at least the largest element in the array;
+int maxSubarrayProduct_1(int a[], int n){
+	if(n==0) return 0;
+	int max_here = a[0];
+	int min_here = a[0];
+	int res = a[0];
+	for(int i=1; i<n; ++i){
+		int tmp = max_here;
+		max_here = max(a[i], max(min_here*a[i], max_here*a[i]));
+		min_here = min(a[i], min(tmp*a[i], min_here*a[i]));
+		res = max_here > res? max_here : res;
+	}
+	return res;
+}
 
 
 int main(){
-	int a1[] = {6, -3, -10, 0, 2};
+	// int a1[] = {6, -3, -10, 0, 2};
+	// int a1[] = {0, -1, 0, -2};   // should be 0; 
+	int a1[] = {-2};
 	int a2[] = {-1, -3, -10, 0, 60};
 	int a3[] = {-2, -3, 0, -2, -40};
-	cout << maxSubarrayProduct(a1, GETSIZE(a1)) << endl;
+	cout << "test 1" << endl;
+ 	cout << maxSubarrayProduct(a1, GETSIZE(a1)) << endl;
+ 	cout << maxSubarrayProduct_1(a1, GETSIZE(a1)) << endl;
+
+	cout << "test 2" << endl;
 	cout << maxSubarrayProduct(a2, GETSIZE(a2)) << endl;
+ 	cout << maxSubarrayProduct_1(a2, GETSIZE(a2)) << endl;
+
+ 	cout << "test 3" << endl;
 	cout << maxSubarrayProduct(a3, GETSIZE(a3)) << endl;
+	cout << maxSubarrayProduct_1(a3, GETSIZE(a3)) << endl;
+
 	return 0;
+
 }
