30958d3da82fa794afa45f9c4a1265e5b6114917
fieldsfarmer <fieldsfarmer@users.noreply.github.com>
2017-12-12 20:12:56 -0500

    Create lower_upper.cpp

---
+++ lower_upper.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <algorithm> // transform
+using namespace std;
+int main(){
+  string a = "123abc";
+  string c = "123ABC";
+  transform(a.begin(), a.end(), a.begin(), ::toupper);
+  transform(c.begin(), c.end(), c.begin(), ::tolower);
+  cout << a << endl;
+  cout << c << endl;
+  return 0;
+}
