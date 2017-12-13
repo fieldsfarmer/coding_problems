ae8fa37bbfd033596b1c3a1f9ac88a8671363a8f
fieldsfarmer <fieldsfarmer@users.noreply.github.com>
2017-12-12 20:18:01 -0500

    Update lower_upper.cpp

--- lower_upper.cpp
+++ lower_upper.cpp
@@ -9,5 +9,8 @@ int main(){
   transform(c.begin(), c.end(), c.begin(), ::tolower);
   cout << a << endl;
   cout << c << endl;
+  char b = 'a';
+  cout << char(::toupper(b)) << endl; // A if no char, then will print out 65
+  
   return 0;
 }
