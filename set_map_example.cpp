14785608d3633354c3537ba8f3e6b206b8411b05
fieldsfarmer <fieldsfarmer@users.noreply.github.com>
2017-12-25 19:36:39 -0500

    Create set_map_example.cpp

---
+++ set_map_example.cpp
@@ -0,0 +1,13 @@
+//map example: http://www.cplusplus.com/reference/map/map/lower_bound/
+//set example: http://www.cplusplus.com/reference/set/set/lower_bound/
+
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+  set<int> s;
+  s.insert(10); s.insert(20); s.insert(30);
+  cout << *(s.lower_bound(10)) << endl; //10
+  cout << *(s.upper_bound(10)) << endl; //20
+  return 0;
+}
